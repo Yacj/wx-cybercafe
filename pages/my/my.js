96144c3a0cd79f396d5582f62596a0c36ad8d773
yacj <1356888297@qq.com>
2020-04-18 17:41:24 +0800

    v1.0

--- pages/my/my.js
+++ pages/my/my.js
@@ -72,41 +72,6 @@ Page({
             url: '/pages/bindCard/bindCard'
         })
     },
-    getTimeInterval(startTime, endTime) {
-        let runTime = parseInt((endTime - startTime) / 1000);
-        let year = Math.floor(runTime / 86400 / 365);
-        runTime = runTime % (86400 * 365);
-        let month = Math.floor(runTime / 86400 / 30);
-        runTime = runTime % (86400 * 30);
-        let day = Math.floor(runTime / 86400);
-        runTime = runTime % 86400;
-        let hour = Math.floor(runTime / 3600);
-        runTime = runTime % 3600;
-        let minute = Math.floor(runTime / 60);
-        runTime = runTime % 60;
-        let second = runTime;
-        let str = '';
-        if (year > 0) {
-            str = year + '年';
-        }
-        if (year <= 0 && month > 0) {
-            str = month + '月';
-        }
-        if (year <= 0 && month <= 0 && day > 0) {
-            str = day + '天';
-        }
-        if (year <= 0 && month <= 0 && day <= 0 && hour > 0) {
-            str = hour + '小时';
-        }
-        if (year <= 0 && month <= 0 && day <= 0 && hour <= 0 && minute > 0) {
-            str = minute + '分钟';
-        }
-        if (year <= 0 && month <= 0 && day <= 0 && hour <= 0 && minute <= 0 && second > 0) {
-            str += second + '秒';
-        }
-        str += '前';
-        return str;
-    },
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
@@ -117,7 +82,6 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-        console.log("2")
         this.onLoad();
         if (typeof this.getTabBar === 'function' &&
             this.getTabBar()) {
